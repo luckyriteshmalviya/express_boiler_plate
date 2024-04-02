0373c66f5bb3997d3a830f7ac232b66aac5c87a3
riteshinheritx <luckyriteshmalviya@gmail.com>
2024-04-02 17:58:12 +0530

    jwt_runned_successfully

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-const {todosRoutes} = require("./src/features/todos/todos.routers.js")
-const {authRoute} = require("./src/features/authentication/auth.routers.js")
+const { todosRoutes } = require("./src/features/todos/todos.routers.js");
+const { authRoute } = require("./src/features/authentication/auth.routers.js");
+const { getToken, decodeToken } = require("./src/services/jwt.services.js");
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("Hello There");
+  const token = getToken();
+  return res.send(`Hello There ${token}`);
 });
 
-app.use("/todos", todosRoutes)
-app.use("/auth", authRoute)
+app.get("/decode", (req, res) => {
+  const { authorization } = req.headers;
+  const auth = authorization;
+  const token = decodeToken(auth);
+  return res.send(token);
+});
+
+app.use("/todos", todosRoutes);
+app.use("/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
--- src/services/jwt.services.js
+++ src/services/jwt.services.js
@@ -1,11 +1,16 @@
-const JWTKEY = require("./../constants/keys/JWTKEY");
-const jwt = require("jsonwebtoken")
-const dataBase = require("./database.services")
+const { JWTKEY } = require("./../constants/keys.js");
+const jwt = require("jsonwebtoken");
+const { dataBase } = require("./database.services");
 
-const getToken = ()=>{
-    return jwt.sign(dataBase, JWTKEY)
-}
+const getToken = () => {
+  return jwt.sign(dataBase, JWTKEY);
+};
+
+const decodeToken = (token) => {
+  return jwt.verify(token, JWTKEY);
+};
 
 module.exports = {
-    getToken
-}
+  getToken,
+  decodeToken,
+};
