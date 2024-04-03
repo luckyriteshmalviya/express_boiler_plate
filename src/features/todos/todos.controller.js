aa6a0d9ae1a71a5666d380f09921266cbed5e9f6
riteshinheritx <luckyriteshmalviya@gmail.com>
2024-04-03 19:53:40 +0530

    added_crud_in_todos

--- src/features/todos/todos.controller.js
+++ src/features/todos/todos.controller.js
@@ -1,23 +1,48 @@
-const { Messages } = require("../../constants/messages");
+const { Messages } = require('../../constants/messages');
 
+let dbArray = [];
 
 const getAllTodos = (req, res) => {
-  return res.send({ message: "This is a Todos list" });
+  return res.send({ message: 'This is a Todos list', data: dbArray });
 };
 
 const createTodo = (req, res) => {
+  const { title, description } = req.body;
+  const newTodo = {
+    todoId: new Date().toString(),
+    todoTitle: title,
+    todoDescription: description,
+  };
+
+  dbArray.push(newTodo);
   res.status(201);
-  return res.send({ message: Messages.todoCreated });
+  return res.send({ message: Messages.todoCreated, data: dbArray });
 };
 
 const updateTodo = (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  for (let i = 0; i < dbArray.length; i++) {
+    if (dbArray[i].todoId === id) {
+      dbArray[i].todoTitle = title;
+      dbArray[i].todoDescription = description;
+      break;
+    }
+  }
   res.status(200);
-  return res.send({ message: Messages.todoUpdated });
+  return res.send({ message: Messages.todoUpdated, data: dbArray });
 };
 
 const deleteTodo = (req, res) => {
+  const { id } = req.params;
+  for (let i = 0; i < dbArray.length; i++) {
+    if (dbArray[i].todoId === id) {
+      dbArray.splice(i, 1);
+    }
+  }
   res.status(200);
-  return res.send({ message: Messages.todoDeleted });
+  return res.send({ message: Messages.todoDeleted, data: dbArray });
 };
 
 module.exports = { getAllTodos, createTodo, updateTodo, deleteTodo };
